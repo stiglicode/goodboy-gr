598cf7d643705e8df74bed503cac07ded2ceb90c
Milan Stiglic <stiglic.231@gmail.com>
2021-09-24 17:10:13 +0200

    Added basic structure & Added submit handler for testing

--- src/view/Main/index.tsx
+++ src/view/Main/index.tsx
@@ -1,11 +1,20 @@
 import React, { ReactChildren } from "react";
 
 const ViewWrapper = (props: { children: ReactChildren }): JSX.Element => {
+	const testSubimt = (e: React.ChangeEvent<HTMLFormElement>): void => {
+		e.preventDefault();
+	};
+
 	return (
-		<div>
-			{React.Children.map(props.children, child => {
-				return child;
-			})}
+		<div className="view-box">
+			<div className="view-box_content">
+				<form onSubmit={testSubimt}>
+					{React.Children.map(props.children, child => {
+						return child;
+					})}
+				</form>
+			</div>
+			<div className="view-box_image"></div>
 		</div>
 	);
 };
