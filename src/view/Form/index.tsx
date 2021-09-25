a488cddd8cbfa774fab1e13a718a413cb3d52215
Milan Stiglic <stiglic.231@gmail.com>
2021-09-25 23:31:33 +0200

    Added static structure of DForm page

--- src/view/Form/index.tsx
+++ src/view/Form/index.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import FormInput from "../../components/Input";
 
 const FormView = (): JSX.Element => {
-	return <div>Form</div>;
+	return (
+		<>
+			<div className="view-box_content--header">
+				<h1>Potrebujeme od Vás zopár&nbsp;informácií</h1>
+			</div>
+			<div className="view-box_content--body">
+				<div className="view-box_content--body_row"></div>
+				<div className="view-box_content--body_row">
+					<h2>O projekte</h2>
+					<div className="form-inputs_wrapper">
+						<FormInput
+							title="Meno"
+							placeholder="Zadajte Vaše meno"
+							name="clientName"
+							type="text"
+							className="form-inputs_wrapper--item"
+						/>
+						<FormInput
+							title="Priezvisko"
+							placeholder="Zadajte Vaše priezvisko"
+							name="clientLastName"
+							type="text"
+							className="form-inputs_wrapper--item"
+						/>
+						<FormInput
+							title="E-mailová adresa"
+							placeholder="Zadajte Váš e-mail"
+							name="clientEmail"
+							type="email"
+							className="form-inputs_wrapper--item"
+						/>
+						<FormInput
+							title="Telefónne číslo"
+							placeholder="900 123 456"
+							name="clientTel"
+							className="form-inputs_wrapper--item form-inputs_wrapper--item_number prefix-SK phone-number"
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="view-box_content--footer">
+				<button type="button" className="custom-btn-light">
+					Späť
+				</button>
+				<button type="button" className="custom-btn-gray">
+					Pokračovať
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default FormView;
