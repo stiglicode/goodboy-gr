ae517ea0f8efe24e895db1da3540848d710509a8
Milan Stiglic <stiglic.231@gmail.com>
2021-09-27 01:04:57 +0200

    Added enums for Scenes and API

--- src/app/state/types/enum.ts
+++ src/app/state/types/enum.ts
@@ -2,3 +2,12 @@ export enum ScenesActionType {
 	NEXT_SCENE = "next",
 	PREVIOUS_SCENE = "previous",
 }
+
+export enum ScenesType {
+	OPTION = "option",
+	FORM = "form",
+}
+
+export enum API {
+	SHELTERS = "shelters_api",
+}
