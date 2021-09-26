445a74913280c81bdc1e6fb81928bf06bbf69826
Milan Stiglic <stiglic.231@gmail.com>
2021-09-27 01:05:46 +0200

    Added types and interfaces for optionScena, formScena and shelters from api

--- src/app/state/types/index.ts
+++ src/app/state/types/index.ts
@@ -1,5 +1,5 @@
 import reducers from "../reducers";
-import { ScenesActionType } from "./enum";
+import { API, ScenesActionType, ScenesType } from "./enum";
 
 export type RootState = ReturnType<typeof reducers>;
 
@@ -15,3 +15,59 @@ interface SceneReducerPrevious<N> {
 }
 
 export type RedcuerAction<T> = SceneReducerNext<T> | SceneReducerPrevious<T>;
+
+// Types for Option scene reducers
+interface OptionSceneO {
+	donateFormat: string;
+	shelterSelection: string;
+	donationValue: string;
+}
+
+interface OptionReducerSave<S> {
+	type: ScenesType.OPTION;
+	payload: {
+		donateFormat: S;
+		shelterSelection: S;
+		donationValue: S;
+	};
+}
+
+export type OptionReducer<S> = OptionReducerSave<S>;
+export type OptionSceneObject = OptionSceneO;
+
+// Types for Option scene reducers
+interface FormSceneO {
+	name: string;
+	last_name: string;
+	email: string;
+	phoneNumber: string;
+}
+
+interface FormReducerSave<S> {
+	type: ScenesType.FORM;
+	payload: {
+		name: S;
+		last_name: S;
+		email: S;
+		phoneNumber: S;
+	};
+}
+
+export type FormReducer<S> = FormReducerSave<S>;
+export type FormSceneObject = FormSceneO;
+
+// Types for API - Shelters
+interface SheltersData {
+	shelters: {
+		id: number;
+		name: string;
+	}[];
+}
+
+interface SheltersReducerSave {
+	type: API.SHELTERS;
+	payload: SheltersData;
+}
+
+export type SheltersReducer = SheltersReducerSave;
+export type SheltersDataObject = SheltersData;
