258813c952a7f4b9732a35da14f6282bf2e6ebcd
Milan Stiglic <stiglic.231@gmail.com>
2021-09-26 15:18:08 +0200

    Added scenes switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./app/state/types";
 // Bootstrap components
 import { Container, Row } from "react-bootstrap";
 // Custom components
@@ -8,15 +10,30 @@ import Progress from "./components/ProgressIndicator";
 import View from "./view/view-entry";
 
 function App(): JSX.Element {
+	const SceneState = useSelector((state: RootState) => state.scene);
+
+	const seceneSwitcher = () => {
+		/* eslint-disable */
+		switch (SceneState) {
+			case 0:
+				return <View.Option />;
+			case 1:
+				return <View.Form />;
+			case 2:
+				return <View.Check />;
+			default:
+				return <View.Option />;
+		}
+		/* eslint-enable */
+	};
+
 	return (
 		<div className="wrapper">
 			<Header />
 			<Container className="container-sm">
 				<Row>
-					<Progress count="3" status="2" />
-					<View.Wrapper>
-						<View.Check />
-					</View.Wrapper>
+					<Progress count="3" status={SceneState} />
+					<View.Wrapper>{seceneSwitcher()}</View.Wrapper>
 				</Row>
 				<Row>
 					<Footer />
