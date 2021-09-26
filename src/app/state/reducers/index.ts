82ce25024875983132f48f713be8146b3e8d0c03
Milan Stiglic <stiglic.231@gmail.com>
2021-09-27 01:03:46 +0200

    Added reducers to cobine function

--- src/app/state/reducers/index.ts
+++ src/app/state/reducers/index.ts
@@ -1,8 +1,14 @@
 import { combineReducers } from "redux";
 import sceneReducer from "./scene-reducer";
+import optionReducer from "./options-reducer";
+import formReducer from "./form-reducer";
+import apiReducer from "./sheltersAPI-reducer";
 
 const reducers = combineReducers({
 	scene: sceneReducer,
+	optionData: optionReducer,
+	formData: formReducer,
+	_API: apiReducer,
 });
 
 export default reducers;
